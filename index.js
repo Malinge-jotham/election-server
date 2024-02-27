f683fc84e49ee6b704f5586aa8fce57c16c93abe
Malinge-jotham <105794518+Malinge-jotham@users.noreply.github.com>
2024-02-27 18:26:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PDFDocument = require('pdfkit');
 const mysql = require('mysql2');
 const cors = require("cors")
 const app = express();
-const port = 3000;
+  const port = process.env.PORT || 3001;
 
 // MySQL Connection
 const db = mysql.createConnection({
